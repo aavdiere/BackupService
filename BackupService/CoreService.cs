87b1e465636a4adef16d1f7d20a111869f19c761
Achim Vandierendonck <achim.ac.vandierendonck@gmail.com>
2016-08-17 22:51:20 +0200

    include all files

---
+++ BackupService/CoreService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackupService {
+    public partial class CoreService : ServiceBase {
+        public CoreService() {
+            InitializeComponent();
+        }
+
+#if DEBUG
+        public void DebugStart() {
+            // TODO: add log
+            OnStart(null);
+        }
+#endif
+
+        protected override void OnStart(string[] args) {
+        }
+
+        protected override void OnStop() {
+        }
+    }
+}
---
+++ BackupService/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackupService {
+    static class Program {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main() {
+#if DEBUG
+            CoreService service = new CoreService();
+            service.DebugStart();
+            System.Threading.Thread.Sleep(System.Threading.Timeout.Infinite);
+#else
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new CoreService()
+            };
+            ServiceBase.Run(ServicesToRun);
+#endif // DEBUG
+        }
+    }
+}
